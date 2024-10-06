d12787c8293e49ae01ce80072b5a5663c7bebb97
木下匠 <takumin4260@gmail.com>
2024-10-06 15:54:19 +0900

    表示修正

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const openPopup = document.getElementById('openPopup');
-  const closePopup = document.getElementById('closePopup');
-  const popupOverlay = document.getElementById('popupOverlay');
-
-  openPopup.addEventListener('click', function() {
-      popupOverlay.style.display = 'block';
+    const openPopup = document.getElementById('openPopup');
+    const openPopupPc = document.getElementById('openPopupPc'); // 追加
+    const closePopup = document.getElementById('closePopup');
+    const popupOverlay = document.getElementById('popupOverlay');
+  
+    const openPopupAction = function() {
+        popupOverlay.style.display = 'block';
+    };
+  
+    openPopup.addEventListener('click', openPopupAction);
+    openPopupPc.addEventListener('click', openPopupAction); // 追加
+  
+    closePopup.addEventListener('click', function() {
+        popupOverlay.style.display = 'none';
+    });
+  
+    popupOverlay.addEventListener('click', function(e) {
+        if (e.target === popupOverlay) {
+            popupOverlay.style.display = 'none';
+        }
+    });
   });
-
-  closePopup.addEventListener('click', function() {
-      popupOverlay.style.display = 'none';
-  });
-
-  popupOverlay.addEventListener('click', function(e) {
-      if (e.target === popupOverlay) {
-          popupOverlay.style.display = 'none';
-      }
-  });
-});
